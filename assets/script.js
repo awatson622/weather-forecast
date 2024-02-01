9ccd7e53c9d4c146a48eb7013462045e23b925bc
awatson622 <147440197+awatson622@users.noreply.github.com>
2024-02-01 15:00:24 -0800

    Update script.js
    updated javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,36 @@
+async function fetchCurrentWeather(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error fetching current weather data:', error);
+    }
+}
+
+
+function fetchWeatherForecast(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error fetching weather forecast data:', error);
+    }
+}
+
+
+function displayCurrentWeather(data) {
+    console.log('Current weather data:', data);
+}
+
+
+function displayWeatherForecast(data) {
+    console.log('Weather forecast data:', data);
+}
+
+const cityName = 'YOUR_CITY_NAME';
+handleCitySearch(cityName);
 
